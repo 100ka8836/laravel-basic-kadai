79dfff9d484573b8347282348832111cf087f0ff
100ka8836 <100ka8836@gmail.com>
2024-08-06 16:28:26 +0900

    課題15

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::get('/', function () {
+    return view('投稿一覧');
+});
+
 Route::get('/posts', [PostController::class, 'index']);
